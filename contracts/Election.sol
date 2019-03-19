79f425ecf00e3483427f88e3a216bb510633fd22
Giorgi Javrishvili <giorgi.javrishvili@gmail.com>
2019-03-19 10:28:49 +0800

    First commit, init smart contract

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Election {
+	// Store candidate
+	// Read candidate
+	string public candidate;
+	// Constructor
+	constructor() public {
+		candidate = "Candidate 1";
+	}
+
+}
